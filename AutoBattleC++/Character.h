490fa3d206f371100cca2837b1b95dc6cdda66b0
GabrielPilan12 <gabrielcpilan12@hotmail.com>
2022-03-07 21:50:29 -0300

    Primeiras Alteracoes do Codigo
    Inicio das Alteracoes do codigo para fazer Funcionar

---
+++ AutoBattleC++/Character.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "Grid.h"
+#include "Character.h"
+#include "Types.h"
+#include <vector>
+
+class Character
+{
+public:
+
+    Character(Types::CharacterClass charcaterClass);
+    ~Character();
+
+    
+    float Health;
+    float BaseDamage;
+    float DamageMultiplier;
+    //public GridBox currentBox;
+    int PlayerIndex;
+    //public Character Target{ get; set; }
+
+    Character* target;
+
+    bool IsDead;
+    char Icon;
+
+    Types::GridBox currentBox;
+
+    bool TakeDamage(float amount);
+
+    int getIndex(std::vector<Types::GridBox*> v, int index);
+
+    void Die();
+
+    void WalkTo(bool CanWalk);
+
+    void StartTurn(Grid* battlefield);
+
+    bool CheckCloseTargets(Grid* battlefield);
+
+    void Attack(Character* target);
+
+
+};
+
